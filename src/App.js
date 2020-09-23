64c59215295ea8ca8c001da01349d5473dacd594
danielle27355 <danielle27355@gmail.com>
2020-09-22 22:50:39 -0400

    styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Badge from "react-bootstrap/Badge";
-import { render } from '@testing-library/react';
+import { findByLabelText, render } from '@testing-library/react';
 let marked = require("marked");
 
 export default class App extends React.Component {
@@ -17,80 +17,40 @@ export default class App extends React.Component {
     }
 
     render(){
-      var inputStyle = {
-        width: "400px",
-        height: "50vh",
-        marginLeft: "auto",
-        marginRight: "auto",
-        padding: "10px",
-      };
-      var outputStyle = {
-        width: "400px",
-        height: "50vh",
-        backgroundColor: "#DCDCDC",
-        marginLeft: "auto",
-        marginRight: "auto",
-        padding:"10px"
-      };
-
       return (
         <div className="App">
+          <div className="title">
+            <h1>
+              Markdown Previewer
+            </h1>
+          </div>
           <div className="container">
-            meow
-            <div className="row mt-4">
-              <div className="col text=center">
-                <h1>
-                  {" "}
-                  <Badge className="text-align-center" variant="light">
-                    Markdown Previewer
-                  </Badge>
-                </h1>
+            <div className="column">
+              <h2>
+                Input
+              </h2>
+              <div className="input">
+                <textarea 
+                  className="input" 
+                  // style={inputStyle} 
+                  value={this.state.markdown}
+                  onChange ={(e) => {
+                    this.updateMarkdown(e.target.value);
+                  }}> 
+                </textarea>
               </div>
             </div>
-            <div className="row mt-4">
-                
-                <div className="col-md-6">
-                  {" "}
-                  <div className="col text-center">
-                    <h4>
-                      <Badge className="text-align-center" variant="secondary">
-                        Markdown Input
-                      </Badge>
-                    </h4>
-                  </div>
-                  <div className="input" style={inputStyle}>
-                    <textarea 
-                      className="input" 
-                      style={inputStyle} 
-                      value={this.state.markdown}
-                      onChange ={(e) => {
-                        this.updateMarkdown(e.target.value);
-                      }}> 
-                    </textarea>
-                  </div>
-                </div>
-    
-                <div className="col-md-6">
-                  {" "}
-                  <div className="col text-center">
-                    <h4>
-                      <Badge className="text-align-center" variant="secondary">
-                        Preview
-                      </Badge>
-                    </h4>
-                  </div>
-                  <div 
-                    style={outputStyle}
-                    dangerouslySetInnerHTML={{
-                      __html: marked(this.state.markdown),
-                    }}
-                  ></div>
-                </div>
-
-              </div>
-           
-            
-    
+            <div className="column">
+              <h2>
+                Output
+              </h2>
+              <div className="output"
+                // style={outputStyle}
+                dangerouslySetInnerHTML={{
+                  __html: marked(this.state.markdown),
+                }}
+              ></div>
+            </div>
           </div>
         </div>
       );
